e7ad85710d35197cff326837a6557b89159ac6ce
ts19830409 <tsoyaleksey19830409@gmail.com>
2026-08-07 00:11:01 +1000

    Add serializers

---
+++ files/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from files.models import File
+
+class FileSerializer(serializers.ModelSerializer):
+    user_login = serializers.ReadOnlyField(source='user.login')
+    file_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = File
+        fields = [
+            'id', 'user', 'user_login', 'file', 'file_url',
+            'name_file', 'size_file', 'content_type', 
+            'description', 'is_public', 'download_count',
+            'share_link', 'uploaded_at'
+        ]
+        read_only_fields = ['user', 'download_count', 'uploaded_at', 'file_url']
+
+    def get_file_url(self, obj):
+        request = self.context.get('request')
+        if request and obj.file:
+            return request.build_absolute_url(obj.file.url)
+        return None
---
+++ users/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from users.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'login', 'email', 'avatar', 'created_at',
+            'files_count', 'total_size', 'success_uploads', 'failed_uploads'
+        ]
+
+        read_only_fields = ['created_at', 'files_count', 'total_size',
+                            'success_uploads', 'failed_uploads']
