cb865870c2c1913971bfd0caf93844aaf25fbfcc
ts19830409 <tsoyaleksey19830409@gmail.com>
2026-08-07 00:42:31 +1000

    Add views and URLs

---
+++ files/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from files.models import File
+from files.serializers import FileSerializer
+
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return File.objects.all()
+        return File.objects.filter(user=self.request.user)
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
